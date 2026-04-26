2343a7aef941f47547c04248e91fc5a2a1a94530
berthcodex <albert.hidata@gmail.com>
2026-04-26 02:45:28 -0500

    Add files via upload

---
+++ src/db/prisma.js
@@ -0,0 +1,8 @@
+// src/db/prisma.js
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error']
+})
+
+export default prisma
